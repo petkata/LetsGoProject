e2e34b23f316cf1117a3dc77659fcc826cf5d5d3
petkata <petyr_yontchev@abv.bg>
2016-02-10 00:40:49 +0200

    OOP design of project

--- engine/DataBase.java
+++ engine/DataBase.java
@@ -7,30 +7,30 @@ import java.util.Set;
 public class DataBase {
 
 	private static DataBase instance = new DataBase();
-	
-	private Set<Event> eventList; 
+
+	private Set<Event> eventList;
 	private Set<User> userList;
-	
-	private DataBase(){
+
+	private DataBase() {
 		this.eventList = new HashSet<Event>();
 		this.userList = new HashSet<User>();
 		System.out.println("DataBase created");
 	}
-	
-	public static DataBase getInstance(){
+
+	public static DataBase getInstance() {
 		return instance;
 	}
 
-	Set<Event> getEventList(){
+	Set<Event> getEventList() {
 		return this.eventList;
 	}
-	
-	Set<User> getUserList(){
+
+	Set<User> getUserList() {
 		return this.userList;
 	}
-	
-	Event getEvent(String eventName){
-		
+
+	Event getEvent(String eventName) {
+
 		for (Event event : this.eventList) {
 			if (event.getName().equals(eventName)) {
 				return event;
@@ -39,16 +39,16 @@ public class DataBase {
 		System.out.println("No such event");
 		return null;
 	}
-	
-	void addEventToDb(Event newEvent){
-		if(this.eventList.add(newEvent)){
+
+	void addEventToDb(Event newEvent) {
+		if (this.eventList.add(newEvent)) {
 			System.out.println("Add to db successful");
 			return;
 		}
 		System.out.println("Add to db failed: possible duplicate");
 	}
-	
-	void removeEventFromDb(String eventName){
+
+	void removeEventFromDb(String eventName) {
 		Iterator<Event> it;
 		it = this.eventList.iterator();
 		while (it.hasNext()) {
@@ -58,23 +58,23 @@ public class DataBase {
 			}
 		}
 	}
-	
-	public void ShowAllEvents(){
-			for (Event event : eventList) {
-				System.out.println(event);
-			}
-			return;
+
+	public void ShowAllEvents() {
+		for (Event event : eventList) {
+			System.out.println(event);
+		}
+		return;
 	}
-	
-	void addUserToDb(User newUser){
+
+	void addUserToDb(User newUser) {
 		this.userList.add(newUser);
 	}
 
-	void removeUserFromDb(User user){
+	void removeUserFromDb(User user) {
 		this.userList.remove(user);
 	}
-	
-	public void showUserList(){
+
+	public void showUserList() {
 		System.out.println(this.userList);
 	}
 }
